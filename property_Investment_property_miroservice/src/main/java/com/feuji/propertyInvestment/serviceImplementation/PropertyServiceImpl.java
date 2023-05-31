c0b84ca801f65cecabb5ce040cea1fb1595da47f
AkashHN <akashgowda0902@gmail.com>
2023-05-31 15:44:22 +0530

    property investment end to end

--- property_Investment_property_miroservice/src/main/java/com/feuji/propertyInvestment/serviceImplementation/PropertyServiceImpl.java
+++ property_Investment_property_miroservice/src/main/java/com/feuji/propertyInvestment/serviceImplementation/PropertyServiceImpl.java
@@ -2,7 +2,6 @@ package com.feuji.propertyInvestment.serviceImplementation;
 
 import java.util.Date;
 import java.util.List;
-import java.util.stream.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
